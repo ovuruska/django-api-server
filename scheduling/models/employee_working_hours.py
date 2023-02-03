adad6f595620b12c024ec55aaa0eae52cfdbe7ab
Oguz Vuruskaner <ovuruska@outlook.com>
2023-02-03 18:11:16 +0300

    Added working hours tests.

--- scheduling/models/employee_working_hours.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class EmployeeWorkingHours(models.Model):
-	employee = models.ForeignKey('employees.Employee', on_delete=models.CASCADE)
-	weekday = models.IntegerField()
-	branch = models.ForeignKey('branches.Branch', on_delete=models.CASCADE)
-	start = models.TimeField()
-	end = models.TimeField()
