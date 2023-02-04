751141bbb9f4fb0baed87d4ddec91be78154f597
Oguz Vuruskaner <ovuruska@outlook.com>
2023-02-04 12:28:20 +0300

    Employee WH model is finalized.

--- scheduling/models/employee_working_hours.py
+++ scheduling/models/employee_working_hours.py
@@ -1,8 +1,20 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 
-class EmployeeWorkingHours(models.Model):
+
+class EmployeeWorkingHour(models.Model):
 	employee = models.ForeignKey('scheduling.Employee', on_delete=models.CASCADE)
 	date = models.DateField()
+
+	"""
+	0	Monday
+	1	Tuesday
+	2	Wednesday
+	3	Thursday
+	4	Friday
+	5	Saturday
+	6	Sunday
+	"""
+	week_day = models.IntegerField(defult = 0,validators=[MinValueValidator(0), MaxValueValidator(6)])
 	branch = models.ForeignKey('scheduling.Branch', on_delete=models.CASCADE)
-	start = models.TimeField()
-	end = models.TimeField()
+	working_hours = models.CharField(max_length=24, validators=[MinLengthValidator(24)],default=24*"0")
