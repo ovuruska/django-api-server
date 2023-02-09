64193a801b7a2a72db5a44e0ee6751a36556fcc7
Oguz Vuruskaner <ovuruska@outlook.com>
2023-02-09 23:23:46 +0300

    Permissions!

---
+++ authorization/permissions.py
@@ -0,0 +1,14 @@
+
+VIEW_ALL_APPOINTMENTS = ("appointments.view_all", "Can view all appointments")
+VIEW_ALL_CUSTOMERS = ("customers.view_all", "Can view all customers")
+VIEW_ALL_EMPLOYEES = ("employees.view_all", "Can view all employees")
+VIEW_ALL_SERVICES = ("services.view_all", "Can view all services")
+VIEW_ALL_DOGS = ("dogs.view_all", "Can view all dogs")
+VIEW_ALL_PAYROLLS = ("payrolls.view_all", "Can view all payrolls")
+
+
+VIEW_SOME_APPOINTMENTS = ("appointments.view_some", "Can view some appointments")
+
+
+
+
