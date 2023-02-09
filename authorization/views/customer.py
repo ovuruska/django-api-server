7ea2b81004c6b79fc47d2d42cfd1c92ccdd773b1
Oguz Vuruskaner <ovuruska@outlook.com>
2023-02-10 00:19:01 +0300

    Added allow any permission group.

--- authorization/views/customer.py
+++ authorization/views/customer.py
@@ -9,30 +9,26 @@ from scheduling.models import Customer
 from scheduling.serializers.auth import UserSerializer, LoginUserSerializer, CreateUserSerializer
 
 
-class CustomerRegisterAPIView(GenericAPIView,PermissionRequiredMixin):
-    serializer_class = CreateUserSerializer
-    permission_classes = [AllowAny]
+class CustomerRegisterAPIView(GenericAPIView, PermissionRequiredMixin):
+	serializer_class = CreateUserSerializer
+	permission_classes = [AllowAny]
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        customer = Customer.objects.create(user=user)
-        return Response({
-            "user": UserSerializer(user, context=self.get_serializer_context()).data,
-            "token": AuthToken.objects.create(user)[1],
-            "profile": model_to_dict(customer)
-        })
+	def post(self, request, *args, **kwargs):
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		user = serializer.save()
+		customer = Customer.objects.create(user=user)
+		return Response({"user": UserSerializer(user, context=self.get_serializer_context()).data,
+			"token": AuthToken.objects.create(user)[1], "profile": model_to_dict(customer)})
+
+
 class CustomerLoginAPIView(GenericAPIView):
-
 	serializer_class = LoginUserSerializer
+
 	def post(self, request, *args, **kwargs):
 		serializer = self.get_serializer(data=request.data)
 		serializer.is_valid(raise_exception=True)
 		user = serializer.validated_data
 		customer = user.customer
-		return Response({
-			"user": UserSerializer(user, context=self.get_serializer_context()).data,
-			"token": AuthToken.objects.create(user)[1],
-			"profile":model_to_dict(customer)
-		})
+		return Response({"user": UserSerializer(user, context=self.get_serializer_context()).data,
+			"token": AuthToken.objects.create(user)[1], "profile": model_to_dict(customer)})
