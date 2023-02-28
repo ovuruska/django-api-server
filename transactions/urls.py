cb24f039e46983cda134ffd9999a41ae13b42953
Oguz Vuruskaner <ovuruska@outlook.com>
2023-02-28 15:56:11 +0300

    Added single transaction fetch.

--- transactions/urls.py
+++ transactions/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('transactions', views.GetTransactionsAPIView.as_view(), name='get_transactions')
+    path('transactions', views.GetTransactionsAPIView.as_view(), name='get_transactions'),
+	path('transaction/<int:pk>', views.GetAppointmentTransactions.as_view(), name='get_transaction')
 ]
--- transactions/views/transaction.py
+++ transactions/views/transaction.py
@@ -5,5 +5,13 @@ from transactions.serializers.transaction import TransactionSerializer
 
 
 class GetTransactionsAPIView(generics.ListAPIView):
-    serializer_class = TransactionSerializer
-    queryset = Transaction.objects.all()
+	serializer_class = TransactionSerializer
+	queryset = Transaction.objects.all()
+
+
+class GetAppointmentTransactions(generics.ListAPIView):
+	serializer_class = TransactionSerializer
+
+	def get_queryset(self):
+		pk = self.kwargs.get("pk")
+		return Transaction.objects.filter(appointment_id=pk)
