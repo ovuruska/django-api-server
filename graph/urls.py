e0431bde1b7db9e5e5c3f0d97c5f2734f4a8470a
Oguz Vuruskaner <ovuruska@outlook.com>
2023-03-02 08:14:26 +0300

    Disabled GraphQL unsafe route.

--- graph/urls.py
+++ graph/urls.py
@@ -5,5 +5,5 @@ from django.conf import settings
 from graph.views.employee import EmployeeGraphQL
 
 urlpatterns = [
-	path("", EmployeeGraphQL.as_view(graphiql=settings.DEBUG)),
+	#path("", EmployeeGraphQL.as_view(graphiql=settings.DEBUG)),
 ]
