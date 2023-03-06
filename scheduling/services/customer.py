49944887f62595fdabc2b3b26bfbd4cf60807628
Oguz Vuruskaner <ovuruska@outlook.com>
2023-03-06 08:05:49 +0300

    Disabled creating new dog and customer if name already exists.

--- scheduling/services/customer.py
+++ scheduling/services/customer.py
@@ -2,7 +2,11 @@ from django.apps import apps
 
 Customer = apps.get_model('scheduling', 'Customer')
 
-def create_customer_with_name(name,**kwargs) -> Customer:
-	customer = Customer(name=name,validated=False,**kwargs)
-	customer.save()
+
+def create_customer_with_name(name, **kwargs) -> Customer:
+	customer = Customer.objects.filter(name=name).first()
+	if customer is None:
+		customer = Customer(name=name, validated=False, **kwargs)
+		customer.save()
+
 	return customer
--- scheduling/services/dog.py
+++ scheduling/services/dog.py
@@ -3,7 +3,11 @@ from django.apps import apps
 Dog = apps.get_model('scheduling', 'Dog')
 Customer = apps.get_model('scheduling', 'Customer')
 
-def create_pet_with_name(owner, dog_name,**kwargs):
-	dog = Dog(name=dog_name, owner=owner,**kwargs)
-	dog.save()
+
+def create_pet_with_name(owner, dog_name, **kwargs):
+	dog = Dog.objects.filter(name=dog_name, owner=owner).first()
+	if dog is None:
+		dog = Dog(name=dog_name, owner=owner, **kwargs)
+		dog.save()
+
 	return dog
