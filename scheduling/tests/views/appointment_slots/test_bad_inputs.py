c3b92e6e9e984e6f489c305b1b4f424fe0291db4
Oguz Vuruskaner <ovuruska@outlook.com>
2023-03-24 22:30:21 +0300

    Added test cases.

---
+++ scheduling/tests/views/appointment_slots/test_bad_inputs.py
@@ -0,0 +1,108 @@
+import datetime
+
+from django.urls import reverse
+
+from common.auth_test_case import CustomerAuthTestCase
+from common.roles import Roles
+from scheduling.models import Branch, Employee
+
+
+class AppointmentSlotTestBaseCase(CustomerAuthTestCase):
+
+	url = reverse("employee_free_times")
+
+
+	def setUp(self):
+		super().setUp()
+
+		self.branch_1 = Branch.objects.create(
+			name='Branch 1',
+			address='Test Address',
+			phone='123456789',
+		)
+		self.branch_2 = Branch.objects.create(
+			name='Branch 2',
+			address='Test Address',
+			phone='123456789',
+		)
+
+		self.employee_1 = Employee.objects.create(
+			name='Employee 1',
+			branch=self.branch_1,
+			role=Roles.EMPLOYEE_FULL_GROOMING,
+		)
+
+		self.employee_2 = Employee.objects.create(
+			name='Employee 2',
+			branch=self.branch_2,
+			role=Roles.EMPLOYEE_FULL_GROOMING,
+		)
+
+		self.employee_3 = Employee.objects.create(
+			name='Employee 3',
+			branch=self.branch_1,
+			role=Roles.EMPLOYEE_WE_WASH,
+		)
+
+		self.employee_4 = Employee.objects.create(
+			name='Employee 4',
+			branch=self.branch_2,
+			role=Roles.EMPLOYEE_WE_WASH,
+		)
+
+		self.url = reverse("employee_free_times")
+
+	@staticmethod
+	def get_now() -> str:
+
+		now = datetime.datetime.now().replace(microsecond=0,second=0)
+		formatted_time = now.strftime("%Y-%m-%d")
+		return formatted_time
+
+	@staticmethod
+	def format_time(time:datetime.datetime) -> str:
+		# 2023-02-01T00:00:00
+		formatted_time = time.strftime("%Y-%m-%d")
+		return formatted_time
+
+	def test_infinite_loop(self):
+
+		payload = {
+			"employees":[],
+			"branches":[],
+			"duration":60,
+			"service_type":"We Wash",
+			"start_date":self.get_now()
+		}
+		response = self.client.post(self.url, **self.customer_headers, data=payload, format='json')
+		self.assertEqual(response.status_code, 200)
+		#
+		self.assertEqual(len(response.data), 0)
+
+	def test_infinite_loop_2(self):
+
+		payload = {
+			"employees":[],
+			"branches":[],
+			"duration":60,
+			"service_type":"Full Grooming",
+			"start_date":self.get_now()
+		}
+		response = self.client.post(self.url, **self.customer_headers, data=payload, format='json')
+		self.assertEqual(response.status_code, 200)
+		#
+		self.assertEqual(len(response.data), 0)
+
+
+	def test_previous_time(self):
+		payload = {
+			"employees":[],
+			"branches":[],
+			"duration":60,
+			"service_type":"Full Grooming",
+			"start_date":self.format_time(datetime.datetime(0,0, 0, 0, 0, 0, 0))
+		}
+		response = self.client.post(self.url, **self.customer_headers, data=payload, format='json')
+		self.assertEqual(response.status_code, 200)
+		#
+		self.assertEqual(len(response.data), 0)
---
+++ scheduling/tests/views/appointment_slots/test_base.py
@@ -0,0 +1,45 @@
+import datetime
+
+from django.urls import reverse
+
+from common.auth_test_case import CustomerAuthTestCase
+from common.roles import Roles
+from scheduling.models import Branch, Employee, EmployeeWorkingHour
+from scheduling.tests.views.appointment_slots.scenario import ScenarioTestCase
+
+
+class AppointmentSlotTestBaseCase(ScenarioTestCase):
+
+	url = reverse("employee_free_times")
+
+	def test_full_grooming(self):
+		payload = {
+			"employees":[],
+			"branches":[],
+			"duration":60,
+			"service_type":"Full Grooming",
+			"start_date":self.format_time(datetime.datetime(0,0, 0, 0, 0, 0, 0))
+		}
+		response = self.client.post(self.url, **self.customer_headers, data=payload, format='json')
+		self.assertEqual(response.status_code, 200)
+		#
+		self.assertEqual(len(response.data), 40)
+		data = response.data
+		# check if the data is sorted
+		self.assertEqual(sorted(data, key=lambda x: x['start']), data)
+
+	def test_we_wash(self):
+		payload = {
+			"employees":[],
+			"branches":[],
+			"duration":60,
+			"service_type":"Full Grooming",
+			"start_date":self.format_time(datetime.datetime(0,0, 0, 0, 0, 0, 0))
+		}
+		response = self.client.post(self.url, **self.customer_headers, data=payload, format='json')
+		self.assertEqual(response.status_code, 200)
+		#
+		self.assertEqual(len(response.data), 40)
+		# check if the data is sorted
+		data =  response.data
+		self.assertEqual(sorted(data, key=lambda x: x['start']), data)
