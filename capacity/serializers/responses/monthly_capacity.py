6f8704275e0b4ceea26ea6fe02e102c599560301
Oguz Vuruskaner <ovuruska@outlook.com>
2023-04-01 18:53:50 +0300

    Feat(Daily available slots)

--- capacity/serializers/responses/monthly_capacity.py
+++ capacity/serializers/responses/monthly_capacity.py
@@ -12,3 +12,6 @@ class MonthlyCapacityResponseSerializer(serializers.Serializer):
 	date = serializers.DateField()
 	morning_capacity = serializers.FloatField()
 	afternoon_capacity = serializers.FloatField()
+
+	class Meta:
+		fields = ('date', 'morning_capacity', 'afternoon_capacity')
