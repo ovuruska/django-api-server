980933c628ec07c730a7076a020f3ddf72d41581
Ege Bağırsakçı <egebagirsakci@sabanciuniv.edu>
2023-04-11 01:27:43 +0300

    Added reset password view

--- authorization/views/customer.py
+++ authorization/views/customer.py
@@ -16,6 +16,12 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from django.http import HttpResponse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from mailchimp3 import MailChimp
 
 class VerifyTokenView(APIView):
 	permission_classes = [IsAuthenticated]
@@ -58,3 +64,36 @@ class CustomerLoginAPIView(GenericAPIView, PermissionRequiredMixin):
 			customer = Customer.objects.get(user=user)
 			return Response({"user": UserSerializer(user, context=self.get_serializer_context()).data,
 			                 "token": AuthToken.objects.create(user)[1], "profile": model_to_dict(customer)})
+
+class CustomerResetPasswordAPIView(GenericAPIView, PermissionRequiredMixin):
+	permission_classes = [AllowAny]
+	def post(self, request, *args, **kwargs):
+		email = request.data.get("email")
+		user = User.objects.get(email=email)
+		if user:
+			token = default_token_generator.make_token(user)
+			uid = urlsafe_base64_encode(force_bytes(user.pk))
+
+			# Create the password reset link
+			password_reset_link = f'{request.scheme}://{request.get_host()}/reset/{uid}/{token}'
+			print(password_reset_link)
+
+			# Send the email using Mailchimp
+			client = MailChimp(mc_api='cb0a4190cb9430ce9c08ffad42faa6fe-us9', mc_user='Quicker MailChimp')
+			data = {
+				'email_address': email,
+				'status': 'subscribed',
+				'merge_fields': {
+					'PASSWORD_RESET_LINK': password_reset_link,
+				},
+			}
+			response = client.lists.members.create('YOUR_LIST_ID', data)
+
+			if response.get('status') == 'subscribed':
+				return HttpResponse(f'Password reset email sent to {email}')
+			else:
+				return HttpResponse(f'Error sending password reset email: {response}')
+
+
+
+
