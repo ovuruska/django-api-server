23c8393819c53c98c1a366ad860e3ea6f23c5221
Oguz Vuruskaner <ovuruska@outlook.com>
2023-04-17 20:32:52 +0300

    Fixed typo

--- customer/urls.py
+++ customer/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
 	path("appointments/upcoming", GetCustomerUpcomingAppts.as_view(), name="customer/appointments/upcoming"),
 	path("appointments/all", GetCustomerAllAppts.as_view(), name="customer/appointments/all"),
 	path("pets/all", GetCustomerPetDetails.as_view(), name="customer/pets/all"),
-	path("pet/creat", CustomerCreatePetView.as_view(), name="customer/pet/create"),
+	path("pet/create", CustomerCreatePetView.as_view(), name="customer/pet/create"),
 	path('appointment/create', CustomerCreateAppointment.as_view(), name="customer/appointment/create"),
 	path('appointment/cancel/<int:appointment_id>', CustomerCancelAppointment.as_view(), name="customer/appointment/cancel"),
 ]
