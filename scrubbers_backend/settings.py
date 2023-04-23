e34625466dfc12712e795cd34ffd36f7dff5dde8
Oguz Vuruskaner <ovuruska@outlook.com>
2023-04-23 18:35:59 +0300

    fix(isoformat error)

--- scrubbers_backend/settings.py
+++ scrubbers_backend/settings.py
@@ -73,18 +73,21 @@ WSGI_APPLICATION = 'scrubbers_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-	'default':
-		{
-			'ENGINE': 'django.db.backends.postgresql',
-			'NAME': os.environ['DB_NAME'],
-			'USER': os.environ['DB_USER'],
-			'PASSWORD': os.environ['DB_PASSWORD'],
-			'HOST': os.environ['DB_HOST'],
-			'PORT': os.environ['DB_PORT'],
-		}
-}
+USE_SQLITE = os.environ.get('USE_SQLITE', None) or 'False'
+if USE_SQLITE == 'True':
+	DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3', }}
+else:
+	DATABASES = {
+		'default':
+			{
+				'ENGINE': 'django.db.backends.postgresql',
+				'NAME': os.environ['DB_NAME'],
+				'USER': os.environ['DB_USER'],
+				'PASSWORD': os.environ['DB_PASSWORD'],
+				'HOST': os.environ['DB_HOST'],
+				'PORT': os.environ['DB_PORT'],
+			}
+	}
 
 
 # Password validation
