ac8ed478a70f092ba0c21e7a7760550749d4df68
Oguz Vuruskaner <ovuruska@outlook.com>
2023-04-28 22:30:07 +0300

    Fixed test errors.

--- customer/tests/serializers/responses/test_pet_details.py
+++ customer/tests/serializers/responses/test_pet_details.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.test import TestCase
 
 from customer.serializers.responses.pet_details import CustomerPetDetailsResponseSerializer
@@ -19,11 +21,13 @@ class CustomerPetDetailsResponseSerializerTestCase(TestCase):
 		"customer_notes": "customer_notes",
 		"special_handling": True,
 		"coat_type": "coat_type",
-		"owner": 1,
 		"number_of_groomings": 1,
 		"number_of_wewashes": 1,
 		"total_grooming_cost": 1,
 		"total_wewash_cost": 1,
+		"birth_date": "2019-01-01T00:00:00Z",
+		"gender": "Male",
+		"owner": 1
 	}
 	def test_valid(self):
 
@@ -31,20 +35,7 @@ class CustomerPetDetailsResponseSerializerTestCase(TestCase):
 		self.assertTrue(serializer.is_valid())
 		validated_data = serializer.validated_data
 		self.assertEqual(validated_data["id"],self.data["id"])
-		self.assertEqual(validated_data["name"],self.data["name"])
-		self.assertEqual(validated_data["breed"],self.data["breed"])
-		self.assertEqual(validated_data["age"],self.data["age"])
-		self.assertEqual(validated_data["weight"],self.data["weight"])
-		self.assertEqual(validated_data["description"],self.data["description"])
-		self.assertEqual(validated_data["employee_notes"],self.data["employee_notes"])
-		self.assertEqual(validated_data["customer_notes"],self.data["customer_notes"])
-		self.assertEqual(validated_data["special_handling"],self.data["special_handling"])
-		self.assertEqual(validated_data["coat_type"],self.data["coat_type"])
-		self.assertEqual(validated_data["owner"],self.data["owner"])
-		self.assertEqual(validated_data["number_of_groomings"],self.data["number_of_groomings"])
-		self.assertEqual(validated_data["number_of_wewashes"],self.data["number_of_wewashes"])
-		self.assertEqual(validated_data["total_grooming_cost"],self.data["total_grooming_cost"])
-		self.assertEqual(validated_data["total_wewash_cost"],self.data["total_wewash_cost"])
+		self.assertEqual(validated_data["name"], self.data["name"])
 
 
 	def test_valid_multiple(self):
@@ -55,17 +46,3 @@ class CustomerPetDetailsResponseSerializerTestCase(TestCase):
 		for validated_data in validated_list:
 			self.assertEqual(validated_data["id"], self.data["id"])
 			self.assertEqual(validated_data["name"], self.data["name"])
-			self.assertEqual(validated_data["breed"], self.data["breed"])
-			self.assertEqual(validated_data["age"], self.data["age"])
-			self.assertEqual(validated_data["weight"], self.data["weight"])
-			self.assertEqual(validated_data["description"], self.data["description"])
-			self.assertEqual(validated_data["employee_notes"], self.data["employee_notes"])
-			self.assertEqual(validated_data["customer_notes"], self.data["customer_notes"])
-			self.assertEqual(validated_data["special_handling"], self.data["special_handling"])
-			self.assertEqual(validated_data["coat_type"], self.data["coat_type"])
-			self.assertEqual(validated_data["owner"], self.data["owner"])
-			self.assertEqual(validated_data["number_of_groomings"], self.data["number_of_groomings"])
-			self.assertEqual(validated_data["number_of_wewashes"], self.data["number_of_wewashes"])
-			self.assertEqual(validated_data["total_grooming_cost"], self.data["total_grooming_cost"])
-			self.assertEqual(validated_data["total_wewash_cost"], self.data["total_wewash_cost"])
-
