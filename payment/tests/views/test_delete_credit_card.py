da0c08047e6492dcce141254869288fdb411994d
Oguz Vuruskaner <ovuruska@outlook.com>
2023-05-19 10:52:02 +0300

    delete card tests are implemented.

--- payment/tests/views/test_delete_credit_card.py
+++ payment/tests/views/test_delete_credit_card.py
@@ -1,61 +1,24 @@
-"""
-import os
-
-from django.apps import apps
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import DeleteView
-from drf_yasg.utils import swagger_auto_schema
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-
+from collections import namedtuple
+from requests import HTTPError
 from payment.services.clover import CloverService
-
-CreditCard = apps.get_model('payment.CreditCard')
-
-class DeleteCreditCardView(DeleteView):
-	permission_classes = [IsAuthenticated()]
-
-	@swagger_auto_schema(
-		responses={204: ''}
-	)
-	def delete(self, request, *args, **kwargs):
-		id = kwargs.get('pk')
-		user = request.user
-		customer = user.customer
-		credit_card = CreditCard.objects.get(id=id, owner=customer)
-		clover_service = CloverService()
-		clover_service.delete_credit_card(credit_card.customer_token, credit_card.card_token)
-		credit_card.delete()
-
-		return Response(status=status.HTTP_204_NO_CONTENT)
-"""
-"""
-	exp_month = models.CharField(max_length=2)
-	exp_year = models.CharField(max_length=4)
-	first6 = models.CharField(max_length=6)
-	last4 = models.CharField(max_length=4)
-	brand = models.CharField(max_length=32)
-	owner = models.ForeignKey('scheduling.Customer', on_delete=models.CASCADE, related_name='credit_cards')
-	customer_token = models.CharField(max_length=100, null=True, blank=True)
-	card_token = models.CharField(max_length=100, null=True, blank=True)
-"""
-
-from django.apps import apps
 from django.urls import reverse
 from unittest.mock import patch
-
 from common.auth_test_case import CustomerAuthTestCase
 
+CreditCardMock = namedtuple('CreditCard', ['customer_token', 'card_token', 'id',  'exp_month', 'exp_year', 'first6', 'last4', 'brand','cvv'])
 
-# Path: payment/tests/views/test_delete_credit_card.py
+class CreditCardWrapper:
+    def __init__(self, credit_card):
+        self.credit_card = credit_card
 
-CreditCard = apps.get_model('payment.CreditCard')
-Customer = apps.get_model('scheduling.Customer')
+    def __getattr__(self, attr):
+        return getattr(self.credit_card, attr)
 
+    def delete(self):
+        # Placeholder delete function
+        pass
 
 class DeleteCreditCardTestCase(CustomerAuthTestCase):
-	url = reverse('payment/delete-card')
 	credit_card_data = {
 		'id':1,
 		'exp_month': '01',
@@ -67,9 +30,45 @@ class DeleteCreditCardTestCase(CustomerAuthTestCase):
 		'customer_token': "customer_1234",
 		'card_token': "card_1234",
 	}
+	credit_card = CreditCardWrapper(CreditCardMock(**credit_card_data))
 
-	def test_delete_customer_credit_card(self):
-		with patch('payment.models.CreditCard.objects.get', return_value=self.credit_card_data):
-			credit_card_id = self.credit_card_data['id']
-			self.url
-			response = self.client.delete(self.url, **self.customer_headers)
+	@patch.object(CloverService, 'delete_credit_card')
+	def test_delete_customer_credit_card(self,mock_delete_credit_card):
+		with patch('payment.models.CreditCard.objects.get', return_value=self.credit_card) as mock_get:
+				credit_card_id = self.credit_card.id
+				url = reverse('payment/delete-card', kwargs={'pk': credit_card_id})
+				with patch.object(self.credit_card, 'delete') as mock_delete:
+					response = self.client.delete(url, **self.customer_headers)
+					self.assertEqual(response.status_code, 204)
+					mock_get.assert_called_once_with(id=credit_card_id, owner=self.customer)
+					mock_delete_credit_card.assert_called_once_with(self.credit_card.customer_token, self.credit_card.card_token)
+					mock_delete.assert_called_once()
+
+
+	@patch.object(CloverService, 'delete_credit_card')
+	def test_delete_customer_credit_card_not_found(self,mock_delete_credit_card):
+		with patch('payment.models.CreditCard.objects.get', return_value=None) as mock_get:
+				credit_card_id = 2
+				url = reverse('payment/delete-card', kwargs={'pk': credit_card_id})
+				with patch.object(self.credit_card, 'delete') as mock_delete:
+					response = self.client.delete(url, **self.customer_headers)
+					self.assertEqual(response.status_code, 404)
+					mock_get.assert_called_once_with(id=credit_card_id, owner=self.customer)
+					mock_delete_credit_card.assert_not_called()
+					mock_delete.assert_not_called()
+					response_data = response.json()
+					self.assertEqual(response_data['detail'], f'Credit card with id {credit_card_id} not found')
+
+	@patch.object(CloverService, 'delete_credit_card', side_effect=HTTPError('Test Exception'))
+	def test_delete_customer_credit_card_exception(self,mock_delete_credit_card):
+		with patch('payment.models.CreditCard.objects.get', return_value=self.credit_card) as mock_get:
+				credit_card_id = self.credit_card.id
+				url = reverse('payment/delete-card', kwargs={'pk': credit_card_id})
+				with patch.object(self.credit_card, 'delete') as mock_delete:
+					response = self.client.delete(url, **self.customer_headers)
+					self.assertEqual(response.status_code, 503)
+					mock_get.assert_called_once_with(id=credit_card_id, owner=self.customer)
+					mock_delete_credit_card.assert_called_once_with(self.credit_card.customer_token, self.credit_card.card_token)
+					mock_delete.assert_not_called()
+					response_data = response.json()
+					self.assertEqual(response_data['detail'], 'Test Exception')
--- payment/views/delete_credit_card.py
+++ payment/views/delete_credit_card.py
@@ -1,30 +1,34 @@
-import os
-
+from requests import HTTPError
 from django.apps import apps
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import DeleteView
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from payment.services.clover import CloverService
 
 CreditCard = apps.get_model('payment.CreditCard')
 
-class DeleteCreditCardView(DeleteView):
-	permission_classes = [IsAuthenticated()]
-
+class DeleteCreditCardView(APIView):
+	permission_classes = [IsAuthenticated]
 	@swagger_auto_schema(
 		responses={204: ''}
 	)
 	def delete(self, request, *args, **kwargs):
-		id = kwargs.get('pk')
-		user = request.user
-		customer = user.customer
-		credit_card = CreditCard.objects.get(id=id, owner=customer)
-		clover_service = CloverService()
-		clover_service.delete_credit_card(credit_card.customer_token, credit_card.card_token)
-		credit_card.delete()
-
+		try:
+			id = kwargs.get('pk')
+			user = request.user
+			customer = user.customer
+			credit_card = CreditCard.objects.get(id=id, owner=customer)
+			assert credit_card is not None
+			clover_service = CloverService()
+			clover_service.delete_credit_card(credit_card.customer_token, credit_card.card_token)
+			credit_card.delete()
+		except AssertionError as e:
+			return Response(status=status.HTTP_404_NOT_FOUND, data={'detail': f'Credit card with id {id} not found'})
+		except HTTPError as e:
+			return Response(status=status.HTTP_503_SERVICE_UNAVAILABLE, data={'detail': str(e)})
+		except Exception as e:
+			return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR, data={'detail': str(e)})
 		return Response(status=status.HTTP_204_NO_CONTENT)
